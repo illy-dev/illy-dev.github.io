77e28e222ef432962095872006cb7847cd196ced
Illy <125080991+illy-dev@users.noreply.github.com>
2024-03-14 19:49:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const trailer = document.getElementById("trailer");
+
+window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth / 2;
+    const y = e.clientY - trailer.offsetHeight / 2;
+
+    trailer.style.transform = `translate(${x}px, ${y}px)`;
+}
