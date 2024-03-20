24f010573ffab4e867a663275278831fa3f1f754
Illy <125080991+illy-dev@users.noreply.github.com>
2024-03-20 08:44:21 +0100

    Update app.js

--- learning-js/app.js
+++ learning-js/app.js
@@ -1,3 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        //console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');            
+        } else {
+            entry.target.classList.remove('show');
+        }
+
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
 const trailer = document.getElementById("trailer");
 
 window.onmousemove = e => {
