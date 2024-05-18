6c5226133a75450a7af1bdec01628079fd12f9b5
Illy <125080991+illy-dev@users.noreply.github.com>
2024-05-18 14:14:59 +0200

    Update Search.jsx

--- src/Header/Search/Search.jsx
+++ src/Header/Search/Search.jsx
@@ -3,7 +3,6 @@ function Search() {
     return(
         <div className=" overflow-hidden font-body">
             <input type="search" name="search" placeholder="Search/Create Article" className=" float-right p-[6px] border-none mt-[8px] mr[16px]"></input>
-            <button type="submit"><i class="fa fa-search"></i></button>
         </div>
     )
 }
