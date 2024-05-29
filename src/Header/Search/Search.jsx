b36a108f64a364d85a43efe52c56e1ac14c9c603
Illy <125080991+illy-dev@users.noreply.github.com>
2024-05-29 17:32:49 +0200

    Delete src directory

--- src/Header/Search/Search.jsx
+++
@@ -1,9 +0,0 @@
-function Search() {
-
-    return(
-        <div className=" overflow-hidden font-body">
-            <input type="search" name="search" placeholder="Search/Create Article" className=" float-right p-[6px] border-none mt-[8px] mr[16px]"></input>
-        </div>
-    )
-}
-export default Search
