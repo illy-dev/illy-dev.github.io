24b9649f0c87a70a9b8055ddb13cecac49dd5a62
Illy <125080991+illy-dev@users.noreply.github.com>
2025-05-14 20:50:19 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
-  output: "export",
-};
-
-export default nextConfig;
+  output: 'export',
+}
+ 
+export default nextConfig
